c14b40f543d634931117842d1d627ea5062a656c
Akongnwi Che Devert <akongnwi.devert@cm.maviance.com>
2019-07-26 10:32:49 +0100

    Widgets and Sidebars

--- wp-app/wp-content/themes/devert/functions.php
+++ wp-app/wp-content/themes/devert/functions.php
@@ -5,9 +5,11 @@
 // Includes
 include(get_template_directory() . '/includes/frontend/enqueue.php');
 include(get_template_directory() . '/includes/setup.php');
+include(get_template_directory() . '/includes/widgets.php');
 
 // Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
 add_action('after_setup_theme', 'ju_setup_theme');
+add_action('widgets_init', 'ju_widgets');
 
 // Shortcodes
