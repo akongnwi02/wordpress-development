d382b9f284dc701bc1926430643ef4bd50f77dc8
Akongnwi Che Devert <akongnwi.devert@cm.maviance.com>
2019-08-16 22:40:32 +0100

    Header Title

--- wp-app/wp-content/themes/devert/header.php
+++ wp-app/wp-content/themes/devert/header.php
@@ -34,11 +34,20 @@
                 <!-- Top Links
                 ============================================= -->
                 <div class="top-links">
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="faqs.html">FAQs</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                    </ul>
+
+                    <?php
+                    if (has_nav_menu('secondary')) {
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'secondary',
+                                'container' => false,
+                                'fallback_cb' => false,
+                                'depth' => 1
+                            )
+                        );
+                    }
+                    ?>
+
                 </div><!-- .top-links end -->
 
             </div>
@@ -72,7 +81,15 @@
             <!-- Logo
             ============================================= -->
             <div id="logo">
-                <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png">Udemy</a>
+                <?php
+                if (has_custom_logo()) {
+                    the_custom_logo();
+                } else {
+                    ?>
+                    <a href="<?php home_url('/'); ?>" class="standard-logo" data-dark-logo="images/logo-dark.png"><?php bloginfo('name'); ?></a>
+                    <?php
+                }
+                ?>
             </div><!-- #logo end -->
 
             <div class="top-advert">
--- wp-app/wp-content/themes/devert/includes/setup.php
+++ wp-app/wp-content/themes/devert/includes/setup.php
@@ -2,6 +2,13 @@
 
 function ju_setup_theme() {
     add_theme_support('post_thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
 
     register_nav_menu('primary', __('Primary Menu', 'devert'));
+    register_nav_menu('secondary', __('Secondary Menu', 'devert'));
+
+    if (function_exists('quads_register_ad')){
+        quads_register_ad( array('location' => 'udemy_header', 'description' => 'Udemy Header position') );
+    }
 }
